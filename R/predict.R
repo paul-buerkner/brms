bac77d4e21387201bf2cc665e7a3ee57b396e6d4
Paul-Christian Buerkner <paul.buerkner@gmail.com>
2015-10-04 22:33:59 +0200

    predict for truncated continuous models

--- R/predict.R
+++ R/predict.R
@@ -11,20 +11,25 @@
 predict_gaussian <- function(n, data, samples, link) {
   sigma <- if (!is.null(samples$sigma)) samples$sigma
            else data$sigma
-  rnorm(nrow(samples$eta), mean = ilink(samples$eta[, n], link), sd = sigma)
+  args <- list(mean = ilink(samples$eta[, n], link), sd = sigma)
+  rng_continuous(rng = nrow(samples$eta), dist = "norm",
+                 args = args, data = data)
 }
 
 predict_student <- function(n, data, samples, link) {
   sigma <- if (!is.null(samples$sigma)) samples$sigma
            else data$sigma
-  rstudent(nrow(samples$eta), df = samples$nu, mu = ilink(samples$eta[, n], link), 
-           sigma = sigma)
+  args <- list(df = samples$nu, mu = ilink(samples$eta[, n], link), sigma = sigma)
+  rng_continuous(rng = nrow(samples$eta), dist = "student",
+                 args = args, data = data)
 }
 
 predict_cauchy <- function(n, data, samples, link) {
   sigma <- if (!is.null(samples$sigma)) samples$sigma
            else data$sigma
-  rstudent(nrow(samples$eta), df = 1, mu = ilink(samples$eta[, n], link), sigma = sigma)
+  args <- list(df = 1, mu = ilink(samples$eta[, n], link), sigma = sigma)
+  rng_continuous(rng = nrow(samples$eta), dist = "student",
+                 args = args, data = data)
 }
 
 predict_lognormal <- function(n, data, samples, link) {
@@ -32,10 +37,13 @@ predict_lognormal <- function(n, data, samples, link) {
   # as 'identity' is the only valid link
   sigma <- if (!is.null(samples$sigma)) samples$sigma
            else data$sigma
-  rlnorm(nrow(samples$eta), meanlog = samples$eta[, n], sdlog = sigma)
+  args <- list(meanlog = samples$eta[, n], sdlog = sigma)
+  rng_continuous(rng = nrow(samples$eta), dist = "lnorm",
+                 args = args, data = data)
 }
 
 predict_multinormal <- function(n, data, samples, link) {
+  # currently no truncation available
   .fun <- function(i) {
     rmultinormal(1, Sigma = samples$Sigma[i, , ],
                  mu = samples$eta[i, seq(n, data$N, data$N_trait)])
@@ -68,17 +76,23 @@ predict_geometric <- function(n, data, samples, link) {
 }
 
 predict_exponential <-  function(n, data, samples, link) {
-  rexp(nrow(samples$eta), rate = ilink(-samples$eta[, n], link))
+  args <- list(rate = ilink(-samples$eta[, n], link))
+  rng_continuous(rng = nrow(samples$eta), dist = "exp",
+                 args = args, data = data)
 }
 
 predict_gamma <- function(n, data, samples, link) {
-  rgamma(nrow(samples$eta), shape = samples$shape,
-         scale = ilink(samples$eta[, n], link) / samples$shape)
+  args <- list(shape = samples$shape,
+               scale = ilink(samples$eta[, n], link) / samples$shape)
+  rng_continuous(rng = nrow(samples$eta), dist = "gamma",
+                 args = args, data = data)
 }
 
 predict_weibull <- function(n, data, samples, link) {
-  rweibull(nrow(samples$eta), shape = samples$shape,
-           scale = 1 / (ilink(-samples$eta[, n] / samples$shape, link)))
+  args <- list(shape = samples$shape,
+               scale = 1 / (ilink(-samples$eta[, n] / samples$shape, link)))
+  rng_continuous(rng = nrow(samples$eta), dist = "weibull",
+                 args = args, data = data)
 }
 
 predict_inverse.gaussian <- function(n, data, samples, link) {
@@ -174,4 +188,32 @@ predict_ordinal <- function(n, data, samples, family, link) {
   p <- pordinal(1:ncat, eta = samples$eta[, n, ], ncat = ncat, 
                 family = family, link = link)
   first_greater(p, target = runif(nrow(samples$eta), min = 0, max = 1))
+}
+
+#---------------loglik helper-functions----------------------------
+
+rng_continuous <- function(nrng, dist, args, data) {
+  # random numbers from (possibly truncated) continuous distributions
+  # Args:
+  #   nrng: number of random values to generate
+  #   dist: name of a distribution for which the functions
+  #         p<dist>, q<dist>, and r<dist> are available
+  #   args: dditional arguments passed to the distribution functions
+  #   data: data initially passed to Stan
+  # Returns:
+  #   a vector of random values samples from the distribution
+  if (is.null(data$lb) && is.null(data$ub)) {
+    # sample as usual
+    rdist <- paste0("r",dist)
+    do.call(rdist, c(nrng, args))
+  } else {
+    # sample from truncated distribution
+    lb <- ifelse(is.null(data$lb), -Inf, data$lb)
+    ub <- ifelse(is.null(data$ub), Inf, data$ub)
+    pdist <- paste0("p",dist)
+    qdist <- paste0("q",dist)
+    p <- do.call(pdist, c(list(c(lb, ub)), args))
+    rng <- list(runif(nrng, min = p[1], max = p[2]))
+    do.call(qdist, c(rng, args))
+  }
 }
