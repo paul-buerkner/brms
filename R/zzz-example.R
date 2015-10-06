a3b2b3432d44804f8d2015a2ffeae7205fe282fe
Paul-Christian Buerkner <paul.buerkner@gmail.com>
2015-10-06 09:39:05 +0200

    deactivate automatic compilation of testing model for github
    it only needs to be activated for testing of S3 methods and CRAN uploads

--- R/zzz-example.R
+++ R/zzz-example.R
@@ -1,8 +1,8 @@
-brmsfit_example <- brm(count ~ log_Age_c + (1|visit),
-                       data = data.frame(count = rpois(236, lambda = 20),
-                                         visit = rep(c(1:4), each = 59),
-                                         log_Age_c = rnorm(236)), 
-                       family = poisson("log"),
-                       prior = c(set_prior("normal(0,5)", class = "b"),
-                                 set_prior("cauchy(0,2)", class = "sd")),
-                       sample.prior = TRUE, n.warmup = 10, n.iter = 50)
+# brmsfit_example <- brm(count ~ log_Age_c + (1|visit),
+#                        data = data.frame(count = rpois(236, lambda = 20),
+#                                          visit = rep(c(1:4), each = 59),
+#                                          log_Age_c = rnorm(236)), 
+#                        family = poisson("log"),
+#                        prior = c(set_prior("normal(0,5)", class = "b"),
+#                                  set_prior("cauchy(0,2)", class = "sd")),
+#                        sample.prior = TRUE, n.warmup = 10, n.iter = 50)
