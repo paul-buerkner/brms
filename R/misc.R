b826bfe0be8810602da0d3d8012b2754647b3aab
Paul-Christian Buerkner <paul.buerkner@gmail.com>
2015-12-12 11:25:05 +0100

    nlist convenience function

--- R/misc.R
+++ R/misc.R
@@ -74,6 +74,23 @@ collapse_lists <- function(ls) {
   out
 }
 
+nlist <- function(...) {
+  # create a named list using object names
+  m <- match.call()
+  dots <- list(...)
+  no_names <- is.null(names(dots))
+  has_name <- if (no_names) FALSE 
+              else nzchar(names(dots))
+  if (all(has_name)) return(dots)
+  nms <- as.character(m)[-1]
+  if (no_names) {
+    names(dots) <- nms
+  } else {
+    names(dots)[!has_name] <- nms[!has_name]
+  }
+  dots
+}
+
 logit <- function(p) {
   # compute the logit
   log(p / (1 - p))
