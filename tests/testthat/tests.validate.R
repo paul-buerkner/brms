paul-buerkner/brms

commit 2d80639a35d4d589860d8d92d43e3e9726110b9d

fix bug in extract_effects occuring for complicated random effects terms
also test that it works correctly now

R/validate.R
@@ -18,12 +18,13 @@ extract_effects <- function(formula, ..., family = NA, check_response = TRUE) {
   if (class(family) == "family") {
     family <- family$family
   }
-  formula <- formula2string(formula)  
-  fixed <- gsub(paste0("\\([^(\\||~)]*\\|[^\\)]*\\)\\+",
-                       "|\\+\\([^(\\||~)]*\\|[^\\)]*\\)",
-                       "|\\([^(\\||~)]*\\|[^\\)]*\\)"), 
-                "", formula)
-  fixed <- gsub("\\|+[^~]*~", "~", fixed)
+  formula <- formula2string(formula) 
+  # formula without addition arguments
+  simple_formula <- gsub("\\|+[^~]*~", "~", formula)
+  fixed <- gsub(paste0("\\([^\\|~]*\\|[^\\)]*\\)\\+",
+                       "|\\+\\([^\\|~]*\\|[^\\)]*\\)",
+                       "|\\([^\\|~]*\\|[^\\)]*\\)"), 
+                "", simple_formula)
   if (substr(fixed, nchar(fixed), nchar(fixed)) == "~") 
     fixed <- paste0(fixed, "1")
   fixed <- formula(fixed)
@@ -33,8 +34,8 @@ extract_effects <- function(formula, ..., family = NA, check_response = TRUE) {
     stop("invalid formula: response variable is missing")
   
   # extract random effects part
-  rg <- gregexpr("\\([^\\|\\)]*\\|[^\\)]*\\)", formula)
-  rg <- unlist(regmatches(formula, rg))
+  rg <- gregexpr("\\([^\\|~]*\\|[^\\)]*\\)", simple_formula)
+  rg <- unlist(regmatches(simple_formula, rg))
   random <- lapply(regmatches(rg, gregexpr("\\([^\\|]*", rg)), 
                    function(r) formula(paste0("~ ",substr(r, 2, nchar(r)))))
   cor <- unlist(lapply(regmatches(rg, gregexpr("\\|[^\\)]*", rg)), 

tests/testthat/tests.validate.R
@@ -51,6 +51,13 @@ test_that("Test that extract_effects handles addition arguments correctly", {
                y ~ z + x + patient + cens)
 })
 
+test_that("Test that extract_effects accepts complicated random terms", {
+  expect_equal(extract_effects(y ~ x + (I(as.numeric(x)-1) | z))$random,
+               list(~I(as.numeric(x) - 1)))
+  expect_equal(extract_effects(y ~ x + (I(exp(x)-1) + I(x/y) | z))$random,
+               list(~I(exp(x)-1) + I(x/y)))
+})
+
 test_that("Test that extract_time returns all desired variables", {
   expect_equal(extract_time(~1), list(time = "", group = "", all = ~1))
   expect_equal(extract_time(~tt), list(time = "tt", group = "", all = ~1 + tt)) 

commit 41bb197406898508ed0f53da3b371995aaa9442e

fix problems in evaluation of random effects terms introduced in 2d80639

R/validate.R
@@ -21,10 +21,9 @@ extract_effects <- function(formula, ..., family = NA, check_response = TRUE) {
   formula <- formula2string(formula) 
   # formula without addition arguments
   simple_formula <- gsub("\\|+[^~]*~", "~", formula)
-  fixed <- gsub(paste0("\\([^\\|~]*\\|[^\\)]*\\)\\+",
-                       "|\\+\\([^\\|~]*\\|[^\\)]*\\)",
-                       "|\\([^\\|~]*\\|[^\\)]*\\)"), 
-                "", simple_formula)
+  re_exprs <- c("\\+\\([^\\|~]*\\|[^\\)]*\\)",
+                "~\\([^\\|~]*\\|[^\\)]*\\)")
+  fixed <- rename(simple_formula, re_exprs, c("", "~"), fixed = FALSE)
   if (substr(fixed, nchar(fixed), nchar(fixed)) == "~") 
     fixed <- paste0(fixed, "1")
   fixed <- formula(fixed)
@@ -34,7 +33,7 @@ extract_effects <- function(formula, ..., family = NA, check_response = TRUE) {
     stop("invalid formula: response variable is missing")
   
   # extract random effects part
-  rg <- gregexpr("\\([^\\|~]*\\|[^\\)]*\\)", simple_formula)
+  rg <- gregexpr(paste0(re_exprs, collapse = "|"), simple_formula)
   rg <- unlist(regmatches(simple_formula, rg))
   random <- lapply(regmatches(rg, gregexpr("\\([^\\|]*", rg)), 
                    function(r) formula(paste0("~ ",substr(r, 2, nchar(r)))))

tests/testthat/tests.validate.R
@@ -5,9 +5,14 @@ test_that("Test that extract_effects finds all variables in very long formulas",
 })
 
 test_that("Test that extract_effects finds all random effects and grouping factors", {
-  expect_equal(extract_effects(y ~ a + (1+x|g1) + x + (1|g2) + z)$random, list(~1 + x, ~1))
-  expect_equal(extract_effects(y ~ (1+x|g1) + x + (1|g2))$group, c("g1", "g2"))
-  expect_equal(extract_effects(y ~ (1+x|g1:g2) + x + (1|g1))$group, c("g1", "g1:g2"))
+  expect_equal(extract_effects(y ~ a + (1+x|g1) + x + (1|g2) + z)$random, 
+              list(~1 + x, ~1))
+  expect_equal(extract_effects(y ~ (1+x|g1) + x + (1|g2))$group,
+               c("g1", "g2"))
+  expect_equal(extract_effects(y ~ (1+x|g1:g2) + (1|g1))$group, 
+               c("g1", "g1:g2"))
+  expect_equal(extract_effects(y ~ (1|g1))$group, c("g1"))
+  expect_equal(extract_effects(y ~ log(x) + (1|g1))$group, c("g1"))
   expect_error(extract_effects(y ~ (1+x|g1/g2) + x + (1|g1)))
 })
 