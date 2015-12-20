ce286d6a33aad8c080f3a7fdab42c03209776131
Paul-Christian Buerkner <paul.buerkner@gmail.com>
2015-12-20 14:45:58 +0100

    increase complexity of example model

--- R/zzz-example.R
+++ R/zzz-example.R
@@ -1,6 +1,6 @@
 # Uncomment the code below to enable automated unit tests of S3 methods
 
-brmsfit_example <- brm(count ~ log_Age_c + offset(Trt_c) + (1|visit),
+brmsfit_example <- brm(count ~ Trt_c + offset(log_Age_c) + (1+Trt_c|visit),
                        data = data.frame(count = rpois(236, lambda = 20),
                                          visit = rep(c(1:4), each = 59),
                                          log_Age_c = rnorm(236),
