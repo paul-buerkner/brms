25cfbebd8797d2caf81847dba6ff4c145c094e1d
Paul-Christian Buerkner <paul.buerkner@gmail.com>
2015-12-20 14:05:42 +0100

    include an offset in the example model

--- R/zzz-example.R
+++ R/zzz-example.R
@@ -1,9 +1,10 @@
 # Uncomment the code below to enable automated unit tests of S3 methods
 
-brmsfit_example <- brm(count ~ log_Age_c + (1|visit),
+brmsfit_example <- brm(count ~ log_Age_c + offset(Trt_c) + (1|visit),
                        data = data.frame(count = rpois(236, lambda = 20),
                                          visit = rep(c(1:4), each = 59),
-                                         log_Age_c = rnorm(236)), 
+                                         log_Age_c = rnorm(236),
+                                         Trt_c = rnorm(236)), 
                        family = poisson("log"),
                        prior = c(set_prior("normal(0,5)", class = "b"),
                                  set_prior("cauchy(0,2)", class = "sd")),
