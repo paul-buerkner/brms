5eb88cab099d7804494149eb6ebe1c319a1e46be
Paul-Christian Buerkner <paul.buerkner@gmail.com>
2016-01-21 19:03:01 +0100

    fix weirdly failing tests when running R CMD CHECK

--- tests/testthat/tests.priors.R
+++ tests/testthat/tests.priors.R
@@ -129,14 +129,17 @@ test_that("handle_special_priors handles horseshoe prior correctly", {
 })
 
 test_that("get_prior finds all classes for which priors can be specified", {
-  expect_equal(get_prior(count ~ log_Base4_c * Trt_c 
-                         + (1|patient) + (1+Trt_c|visit),
-                         data = epilepsy, family = "poisson")$class,
-               c(rep("b", 5), c("cor", "cor"), "Intercept", rep("sd", 6)))
-  expect_equal(get_prior(rating ~ treat + period, partial = ~ carry, 
+  expect_equal(sort(get_prior(count ~ log_Base4_c * Trt_c 
+                              + (1|patient) + (1+Trt_c|visit),
+                              data = epilepsy, family = "poisson")$class),
+               sort(c(rep("b", 5), c("cor", "cor"), "Intercept", rep("sd", 6))))
+  print(get_prior(rating ~ treat + period, partial = ~ carry, 
+                  data = inhaler, family = "sratio", 
+                  threshold = "equidistant"))
+  expect_equal(sort(get_prior(rating ~ treat + period, partial = ~ carry, 
                          data = inhaler, family = "sratio", 
-                         threshold = "equidistant")$class,
-               c(rep("b", 5), "delta", "Intercept"))
+                         threshold = "equidistant")$class),
+               sort(c(rep("b", 5), "delta", "Intercept")))
 })
 
 test_that("update_prior produces correct prior_frames", {
