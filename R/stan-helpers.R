023628bef5ce9c09d3fcc01f7f3868540c60dd62
Paul-Christian Buerkner <paul.buerkner@gmail.com>
2016-02-06 14:15:17 +0100

    allow non-linear formulae with commas

--- R/stan-helpers.R
+++ R/stan-helpers.R
@@ -473,7 +473,7 @@ stan_nonlinear <- function(effects, data, family = gaussian(),
       new_covars <- paste0(" C[n, ", seq_along(covars), "] ")
     } else new_covars <- NULL
     # add whitespaces to be able to replace parameters and covariates
-    meta_sym <- c("+", "-", "*", "/", "^", ")", "(")
+    meta_sym <- c("+", "-", "*", "/", "^", ")", "(", ",")
     nlmodel <- gsub(" ", "", deparse(effects$fixed[[3]]))
     nlmodel <- wsp(rename(nlmodel, meta_sym, wsp(meta_sym))) 
     nlmodel <- rename(nlmodel, c(nlpars, covars, " ( ", " ) "), 
