4c843bea2421416872fd39b75dfd03eaac6e3c5c
Paul-Christian Buerkner <paul.buerkner@gmail.com>
2016-02-07 21:33:55 +0100

    split up tests of Stan code and data generating functions into multiple R files

---
+++ tests/testthat/tests.data-helpers.R
@@ -0,0 +1,109 @@
+test_that("melt_data returns data in long format", {
+  data <- data.frame(x = rep(c("a","b"), 5), y1 = 1:10, y2 = 11:20, 
+                     y3 = 21:30, z = 100:91)
+  effects <- extract_effects(y ~ x, family = "poisson")
+  expect_equal(melt_data(data, effects = effects, 
+                         family = "poisson"), data)
+  
+  target1 <- data.frame(x = rep(c("a","b"), 10), y1 = rep(1:10, 2), 
+                        y2 = rep(11:20, 2), y3 = rep(21:30, 2),
+                        z = rep(100:91, 2), 
+                        trait = factor(rep(c("y3", "y1"), each = 10),
+                                       levels = c("y3", "y1")), 
+                        response = c(21:30, 1:10))
+  effects <- extract_effects(cbind(y3,y1) ~ x, family = "gaussian")
+  expect_equal(melt_data(data, effects = effects, family = "gaussian"), 
+               target1[, c("x", "y1", "y3", "trait", "response")])
+  
+  target2 <- data.frame(x = rep(c("a","b"), 15), y1 = rep(1:10, 3), 
+                        y2 = rep(11:20, 3), y3 = rep(21:30, 3),
+                        z = rep(100:91, 3),
+                        trait = factor(rep(c("y2", "y1", "y3"), each = 10),
+                                       levels = c("y2", "y1", "y3")), 
+                        response = c(11:20, 1:10, 21:30))
+  effects <- extract_effects(cbind(y2,y1,y3) ~ x, family = "gaussian")
+  expect_equal(melt_data(data, effects = effects, family = "gaussian"), 
+               target2[, c("x", "y1", "y2", "y3", "trait", "response")])
+})
+
+test_that("melt_data returns expected errors", {
+  ee <- extract_effects(y1 ~ x:main, family = hurdle_poisson())
+  data <- data.frame(y1 = rnorm(10), y2 = rnorm(10), x = 1:10)
+  expect_error(melt_data(data = NULL, family = hurdle_poisson(), effects = ee),
+               "data must be a data.frame for multivariate models", 
+               fixed = TRUE)
+  data$main <- 1:10 
+  expect_error(melt_data(data = data, family = hurdle_poisson(), effects = ee),
+               "main is a resevered variable name", 
+               fixed = TRUE)
+  data$response <- 1:10 
+  ee <- extract_effects(response ~ x:main, family = hurdle_poisson())
+  expect_error(melt_data(data = data, family = hurdle_poisson(), effects = ee),
+               "response is a resevered variable name in multivariate models", 
+               fixed = TRUE)
+  data$trait <- 1:10 
+  ee <- extract_effects(y ~ 0 + x*trait, family = hurdle_poisson())
+  expect_error(melt_data(data = data, family = hurdle_poisson(), effects = ee),
+               "trait is a resevered variable name in multivariate models", 
+               fixed = TRUE)
+  
+  ee <- extract_effects(cbind(y1, y2) ~ x)
+  data <- data.frame(y1 = rnorm(10), y2 = rnorm(10), x = 1:10)
+  expect_error(melt_data(data = data, family = poisson(), effects = ee),
+               "invalid multivariate model", fixed = TRUE)
+})
+
+test_that("combine_groups does the expected", {
+  data <- data.frame(x = rep(c("a","b"), 5), y1 = 1:10, 
+                     y2 = 11:20, y3 = 21:30, z = 100:91)
+  expected <- data 
+  expected[["y1:y2"]] <- paste0(data$y1, "_", data$y2)
+  expected[["y1:y2:y3"]] <- paste0(data$y1, "_", data$y2, "_", data$y3)
+  expect_equal(combine_groups(data, "y1:y2", "y1:y2:y3"), expected)
+})
+
+test_that("get_model_matrix removes intercepts correctly", {
+  data <- data.frame(x = factor(rep(1:2, 5)), y = 11:20)
+  expect_equal(get_model_matrix(y ~ x, data, rm_intercept = TRUE),
+               structure(matrix(rep(0:1, 5)), dimnames = list(1:10, "x2")))
+})
+
+test_that(paste("arr_design_matrix returns correct design", 
+                "matrices for autoregressive effects"), {
+  expect_equal(arr_design_matrix(1:10, 0, sort(rep(1:2, 5))), NULL)
+  expect_equal(arr_design_matrix(1:10, 1, sort(rep(1:2, 5))), 
+               matrix(c(0,1:4.5,0,6:9.5)))
+  expect_equal(arr_design_matrix(1:10, 2, sort(rep(1:2, 5))), 
+               cbind(c(0,1:4.5,0,6:9), c(0,0,1:3,0,0,6:8)))
+})
+
+
+test_that("amend_newdata handles factors correctly", {
+  fit <- rename_pars(brmsfit_example)
+  fit$data$fac <- factor(sample(1:3, nrow(fit$data), replace = TRUE))
+  newdata <- fit$data[1:5, ]
+  expect_silent(amend_newdata(newdata, fit))
+  newdata$visit <- 1:5
+  expect_error(amend_newdata(newdata, fit), fixed = TRUE,
+               "levels 5 of grouping factor visit not found")
+  newdata$fac <- 1:5
+  expect_error(amend_newdata(newdata, fit), fixed = TRUE,
+               "New factor levels are not allowed")
+})
+
+test_that("update_data handles NAs correctly", {
+  data <- data.frame(y1 = c(1, NA, 3), y2 = 4:6, x = 10:12, z = NA)
+  effects <- extract_effects(cbind(y1, y2) ~ x, family = "gaussian")
+  expect_equivalent(update_data(data, family = "gaussian", effects = effects),
+                    data.frame(response = c(1, 3, 4, 6), y1 = c(1, 3, 1, 3), 
+                               y2 = c(4, 6, 4, 6), x = c(10, 12, 10, 12)))
+  effects <- extract_effects(y1 ~ x, family = "hurdle_gamma")
+  expect_equivalent(update_data(data, family = "hurdle_gamma", effects = effects),
+                    data.frame(response = c(1, 3, 1, 3), y1 = c(1, 3, 1, 3),
+                               x = c(10, 12, 10, 12)))
+  effects <- extract_effects(y2 ~ x, family = "zero_inflated_poisson")
+  expect_equivalent(update_data(data, family = "zero_inflated_poisson", 
+                                effects = effects),
+                    data.frame(response = c(4:6, 4:6), y2 = c(4:6, 4:6),
+                               x = c(10:12, 10:12)))
+})
--- tests/testthat/tests.stan-helpers.R
+++ tests/testthat/tests.stan-helpers.R
@@ -47,17 +47,6 @@ test_that("stan_prior passes increment_log_prob statements without changes", {
                "  increment_log_prob(a); \n  increment_log_prob(b); \n")
 })
 
-test_that("make_stancode handles horseshoe priors correctly", {
-  prior <- prior_frame(prior = "normal(0, hs_local * hs_global", class = "b")
-  attr(prior, "hs_df") <- 7
-  temp_stancode <- make_stancode(rating ~ treat*period*carry, data = inhaler,
-                                 prior = prior)
-  expect_match(temp_stancode, fixed = TRUE,
-    "  vector<lower=0>[K] hs_local; \n  real<lower=0> hs_global; \n")
-  expect_match(temp_stancode, fixed = TRUE,
-    "  hs_local ~ student_t(7, 0, 1); \n  hs_global ~ cauchy(0, 1); \n")
-})
-
 test_that("stan_eta returns correct strings for autocorrelation models", {
   expect_match(stan_eta(family = student(log), f = c("Trt_c"),
                         autocor = cor_arma(~visit|patient, p = 2))$transC3,
@@ -70,7 +59,7 @@ test_that("stan_eta returns correct strings for autocorrelation models", {
                "eta <- X * b + temp_Intercept + Yarr * arr", fixed = TRUE)
 })
 
-test_that("Test_that stan_arma returns correct strings (or errors)", {
+test_that("stan_arma returns correct strings (or errors)", {
   expect_equal(stan_arma(family = gaussian(log), 
                          autocor = cor.arma()), list())
   prior <- c(set_prior("normal(0,2)", class = "ar"),
@@ -99,91 +88,6 @@ test_that("Test_that stan_arma returns correct strings (or errors)", {
                          autocor = cor.arma(~visit|patient, p = 1, q = 1)),
                "ARMA effects for family poisson are not yet implemented")
 })  
-
-test_that("make_stancode accepts supported links", {
-  expect_match(make_stancode(rating ~ treat + period + carry, 
-                             data = inhaler, family = sratio("probit_approx")), 
-               "Phi_approx")
-  expect_match(make_stancode(rating ~ treat + period + carry, 
-                             data = inhaler, family = c("cumulative", "probit")), 
-               "Phi")
-  expect_match(make_stancode(rating ~ treat + period + carry, 
-                                 data = inhaler, family = "poisson"), 
-               "log")
-})
-
-test_that(paste("make_stancode returns correct strings", 
-                "for customized covariances"), {
-  expect_match(make_stancode(rating ~ treat + period + carry + (1|subject), 
-                             data = inhaler, cov_ranef = list(subject = 1)), 
-              "r_1 <- sd_1 * (cov_1 * pre_1)", fixed = TRUE)
-  expect_match(make_stancode(rating ~ treat + period + carry + (1+carry|subject), 
-                             data = inhaler, cov_ranef = list(subject = 1)),
-               paste0("r_1 <- to_array(kronecker_cholesky(cov_1, L_1, sd_1) * ",
-                      "to_vector(pre_1), N_1, K_1"),
-               fixed = TRUE)
-  expect_match(make_stancode(rating ~ treat + period + carry + (1+carry||subject), 
-                             data = inhaler, cov_ranef = list(subject = 1)), 
-               paste0("  r_1_1 <- sd_1[1] * (cov_1 * pre_1[1]);  // scale REs \n",
-                      "  r_1_2 <- sd_1[2] * (cov_1 * pre_1[2]);"),
-               fixed = TRUE)
-})
-
-test_that("make_stancode handles addition arguments correctly", {
-  expect_match(make_stancode(time | cens(censored) ~ age + sex + disease, 
-                             data = kidney, family = c("weibull", "log")), 
-               "vector[N] cens;", fixed = TRUE)
-  expect_match(make_stancode(time | trunc(0) ~ age + sex + disease,
-                             data = kidney, family = "gamma"), 
-               "T[lb, ];", fixed = TRUE)
-  expect_match(make_stancode(time | trunc(ub = 100) ~ age + sex + disease, 
-                             data = kidney, family = cauchy("log")), 
-               "T[, ub];", fixed = TRUE)
-  expect_match(make_stancode(count | trunc(0, 150) ~ Trt_c, 
-                             data = epilepsy, family = "poisson"), 
-               "T[lb, ub];", fixed = TRUE)
-})
-
-test_that("make_stancode correctly combines strings of multiple grouping factors", {
-  expect_match(make_stancode(count ~ (1|patient) + (1+Trt_c|visit), 
-                             data = epilepsy, family = "poisson"), 
-               paste0("  real Z_1[N];  // RE design matrix \n",
-                      "  // data for random effects of visit \n"), 
-               fixed = TRUE)
-  expect_match(make_stancode(count ~ (1|visit) + (1+Trt_c|patient), 
-                             data = epilepsy, family = "poisson"), 
-               paste0("  int NC_1;  // number of correlations \n",
-                      "  // data for random effects of visit \n"), 
-               fixed = TRUE)
-})
-
-test_that("make_stancode handles models without fixed effects correctly", {
-  expect_match(make_stancode(count ~ 0 + (1|patient) + (1+Trt_c|visit), 
-                             data = epilepsy, family = "poisson"), 
-               "  eta <- rep_vector(0, N); \n", fixed = TRUE)
-})
-
-test_that("make_stancode returns expected code for 2PL models", {
-  data <- data.frame(y = rep(0:1, each = 5), x = rnorm(10))
-  stancode <- make_stancode(y ~ x, data = data, 
-                            family = bernoulli(type = "2PL"))
-  expect_match(stancode, paste0("eta_2PL <- head(eta, N_trait)", 
-                                " .* exp(tail(eta, N_trait))"),
-               fixed = TRUE)
-  expect_match(stancode, "Y ~ bernoulli_logit(eta_2PL);",
-               fixed = TRUE)
-})
-
-test_that("make_stancode correctly restricts FE parameters", {
-  data <- data.frame(y = rep(0:1, each = 5), x = rnorm(10))
-  sc <- make_stancode(y ~ x, data, prior = set_prior("", lb = 2))
-  expect_match(sc, "vector<lower=2>[K] b", fixed = TRUE)
-  sc <- make_stancode(y ~ x, data, prior = set_prior("p", ub = "4"))
-  expect_match(sc, "vector<upper=4>[K] b", fixed = TRUE)
-  prior <- set_prior("normal(0,5)", lb = "-3", ub = 5)
-  sc <- make_stancode(y ~ x, data, prior = prior)
-  expect_match(sc, "vector<lower=-3,upper=5>[K] b", fixed = TRUE)
-})
 
 test_that("stan_ordinal returns correct strings", {
   expect_match(stan_ordinal(family = sratio())$par, "")
@@ -302,64 +206,6 @@ test_that("stan_rngprior returns correct sampling statements for priors", {
   
 })
 
-test_that("make_stancode returns correct selfmade functions", {
-  # cauchit link
-  expect_match(make_stancode(rating ~ treat, data = inhaler,
-                             family = cumulative("cauchit")),
-               "real inv_cauchit(real y)", fixed = TRUE)
-  # inverse gaussian models
-  temp_stancode <- make_stancode(time | cens(censored) ~ age, data = kidney,
-                                 family = inverse.gaussian)
-  expect_match(temp_stancode, "real inv_gaussian_log(real y", fixed = TRUE)
-  expect_match(temp_stancode, "real inv_gaussian_cdf_log(real y", fixed = TRUE)
-  expect_match(temp_stancode, "real inv_gaussian_ccdf_log(real y", fixed = TRUE)
-  expect_match(make_stancode(time ~ 1, data = kidney, family = inverse.gaussian),
-               "real inv_gaussian_log(vector y", fixed = TRUE)
-  # zero-inflated and hurdle models
-  expect_match(make_stancode(count ~ Trt_c, data = epilepsy, 
-                             family = "zero_inflated_poisson"),
-               "real zero_inflated_poisson_log(int y", fixed = TRUE)
-  expect_match(make_stancode(count ~ Trt_c, data = epilepsy, 
-                             family = "zero_inflated_negbinomial"),
-               "real zero_inflated_neg_binomial_2_log(int y", fixed = TRUE)
-  expect_match(make_stancode(count ~ Trt_c, data = epilepsy, 
-                             family = "zero_inflated_binomial"),
-               "real zero_inflated_binomial_log(int y", fixed = TRUE)
-  expect_match(make_stancode(count ~ Trt_c, data = epilepsy, 
-                             family = "zero_inflated_beta"),
-               "real zero_inflated_beta_log(real y", fixed = TRUE)
-  expect_match(make_stancode(count ~ Trt_c, data = epilepsy, 
-                             family = hurdle_poisson()),
-               "real hurdle_poisson_log(int y", fixed = TRUE)
-  expect_match(make_stancode(count ~ Trt_c, data = epilepsy, 
-                             family = hurdle_negbinomial),
-               "real hurdle_neg_binomial_2_log(int y", fixed = TRUE)
-  expect_match(make_stancode(count ~ Trt_c, data = epilepsy, 
-                             family = hurdle_gamma("log")),
-               "real hurdle_gamma_log(real y", fixed = TRUE)
-  # linear models with special covariance structures
-  expect_match(make_stancode(rating ~ treat, data = inhaler, 
-                             autocor = cor_ma(cov = TRUE)),
-               "real normal_cov_log(vector y", fixed = TRUE)
-  expect_match(make_stancode(time ~ age, data = kidney, family = "student", 
-                             autocor = cor_ar(cov = TRUE)),
-               "real student_t_cov_log(vector y", fixed = TRUE)
-  # ARMA covariance matrices
-  expect_match(make_stancode(rating ~ treat, data = inhaler, 
-                             autocor = cor_ar(cov = TRUE)),
-               "matrix cov_matrix_ar1(real ar", fixed = TRUE)
-  expect_match(make_stancode(time ~ age, data = kidney, family = "student", 
-                             autocor = cor_ma(cov = TRUE)),
-               "matrix cov_matrix_ma1(real ma", fixed = TRUE)
-  expect_match(make_stancode(time ~ age, data = kidney, family = "cauchy", 
-                             autocor = cor_arma(p = 1, q = 1, cov = TRUE)),
-               "matrix cov_matrix_arma1(real ar, real ma", fixed = TRUE)
-  # kronecker matrices
-  expect_match(make_stancode(rating ~ treat + period + carry + (1+carry|subject), 
-                             data = inhaler, cov_ranef = list(subject = 1)), 
-               "matrix kronecker_cholesky.*vector\\[\\] to_array")
-})
-
 test_that("stan_multi returns correct Stan code (or errors)", {
   expect_equal(stan_multi(gaussian(), "y"), list())
   expect_match(stan_multi(gaussian(), c("y1", "y2"))$transC, 
@@ -370,40 +216,4 @@ test_that("stan_multi returns correct Stan code (or errors)", {
   expect_equal(stan_multi(hurdle_gamma(), c("y", "huy")), list())
   expect_error(stan_multi(poisson(), c("y1", "y2")),
                "invalid multivariate model")
-})
-
-test_that("make_stancode detects invalid combinations of modeling options", {
-  data <- data.frame(y1 = rnorm(10), y2 = rnorm(10), 
-                     wi = 1:10, ci = sample(-1:1, 10, TRUE))
-  expect_error(make_stancode(y1 | cens(ci) ~ y2, data = data,
-                             autocor = cor_ar(cov = TRUE)),
-               "Invalid addition arguments")
-  expect_error(make_stancode(cbind(y1, y2) ~ 1, data = data,
-                             autocor = cor_ar(cov = TRUE)),
-               "multivariate models are not yet allowed")
-  expect_error(make_stancode(y1 | se(wi) ~ y2, data = data,
-                             autocor = cor_ma()),
-               "Please set cov = TRUE", fixed = TRUE)
-  expect_error(make_stancode(y1 | trunc(lb = -50) | weights(wi) ~ y2,
-                             data = data),
-               "truncation is not yet possible")
-})
-
-test_that("make_stancode is silent for multivariate models", {
-  data <- data.frame(y1 = rnorm(10), y2 = rnorm(10), x = 1:10)
-  expect_silent(make_stancode(cbind(y1, y2) ~ x, data = data))
-})
-
-test_that("make_stancode is silent for categorical models", {
-  data <- data.frame(y = sample(1:4, 10, TRUE), x = 1:10)
-  expect_silent(make_stancode(y ~ x, data = data, family = categorical()))
-})
-
-test_that("make_stancode returns correct code for intercept only models", {
-  expect_match(make_stancode(rating ~ 1, data = inhaler),
-               "b_Intercept <- temp_Intercept;", fixed = TRUE) 
-  expect_match(make_stancode(rating ~ 1, data = inhaler, family = sratio()),
-               "b_Intercept <- temp_Intercept;", fixed = TRUE) 
-  expect_match(make_stancode(rating ~ 1, data = inhaler, family = categorical()),
-               "b_Intercept <- temp_Intercept;", fixed = TRUE) 
 })
