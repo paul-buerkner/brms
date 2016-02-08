07822e3c3a9661ee843c32fb9b915e2fff3ea8f0
Paul-Christian Buerkner <paul.buerkner@gmail.com>
2016-02-09 00:23:33 +0100

    brmsfit_example: only run 2 chains

--- R/zzz-example.R
+++ R/zzz-example.R
@@ -10,7 +10,7 @@ brmsfit_example <- brm(count ~ Trt_c + offset(log_Age_c) + (1+Trt_c|visit),
                        autocor = cor_arma(~visit|patient, 1, 1),
                        prior = c(set_prior("normal(0,5)", class = "b"),
                                  set_prior("cauchy(0,2)", class = "sd")),
-                       warmup = 10, iter = 40, testmode = TRUE)
+                       warmup = 10, iter = 40, chains = 2, testmode = TRUE)
 brmsfit_example$fit@stanmodel <- new("stanmodel")
 
 
