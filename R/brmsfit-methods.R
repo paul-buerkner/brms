f0f758f9312062c12caa6b538ecb7902a96554cc
Paul-Christian Buerkner <paul.buerkner@gmail.com>
2016-03-24 12:35:26 +0100

    update.brmsfit: allow to update the 'sparse' argument

--- R/brmsfit-methods.R
+++ R/brmsfit-methods.R
@@ -1527,8 +1527,8 @@ update.brmsfit <- function(object, formula., newdata = NULL, ...) {
   }
 
   object$prior <- update_prior_frame(object$prior, ranef = object$ranef)
-  rc_args <- c("family", "prior", "autocor", "nonlinear", 
-               "partial", "threshold", "cov_ranef", "sample_prior")
+  rc_args <- c("family", "prior", "autocor", "nonlinear", "partial", 
+               "threshold", "cov_ranef", "sparse", "sample_prior")
   new_args <- intersect(rc_args, names(dots))
   recompile <- recompile || length(new_args)
   if (recompile) {
