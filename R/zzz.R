f82992a30c57d1e1c1f74bf7180f81c8cecd8b66
Paul-Christian Bürkner <paul.buerkner@gmail.com>
2016-09-13 23:46:30 +0200

    move to precompiled example models stored in R/sysdata.rda

--- R/zzz.R
+++ R/zzz.R
@@ -1,21 +1,3 @@
-# Uncomment the code below to enable unit tests of S3 methods
-
-brmsfit_example <- brm(bf(count ~ Trt*Age + mono(Exp) + s(Age) +
-                          offset(Age) + (1+Trt|visit), sigma ~ Trt),
-                       data = data.frame(count = rpois(236, lambda = 20),
-                                         visit = rep(1:4, each = 59),
-                                         patient = factor(rep(1:59, 4)),
-                                         Age = rnorm(236), Trt = rnorm(236),
-                                         Exp = sample(1:5, 236, TRUE)),
-                       family = student(), sample_prior = TRUE,
-                       autocor = cor_arma(~visit|patient, 1, 1),
-                       prior = c(set_prior("normal(0,5)", class = "b"),
-                                 set_prior("cauchy(0,2)", class = "sd")),
-                       warmup = 150, iter = 200, chains = 2,
-                       save_dso = FALSE, testmode = TRUE)
-brmsfit_example$fit@stanmodel <- new("stanmodel")
-
-
 # Uncomment the code below to enable unit tests for new stan functions
 
 # new_stan_functions <- function() {
