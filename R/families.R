e997460b30584dc736f11e86987a11bd677b859f
Paul-Christian Bürkner <paul.buerkner@gmail.com>
2017-03-18 14:11:31 +0100

    improved print methods for 'brmsfamily' and 'mixfamily' objects

--- R/families.R
+++ R/families.R
@@ -599,23 +599,54 @@ auxpar_family.mixfamily <- function(family, auxpar, ...) {
 }
 
 #' @export
-print.brmsfamily <- function(x, links = FALSE, ...) {
-  cat("\nFamily:", x$family, "\n")
-  cat("Link function:", x$link, "\n")
-  if (!is.null(x$type)) {
-    cat("Type:", x$type, "\n") 
-  }
-  if (isTRUE(links) || is.character(links)) {
-    ap_links <- x[grepl("^link_", names(x))]
-    names(ap_links) <- sub("^link_", "", names(ap_links))
-    if (is.character(links)) {
-      ap_links <- rmNULL(ap_links[links])
+print.brmsfamily <- function(x, links = FALSE, summary = FALSE, 
+                             newline = TRUE, ...) {
+  if (summary) {
+    cat(paste0(x$family, "(", x$link, ")"))
+  } else {
+    cat("\nFamily:", x$family, "\n")
+    cat("Link function:", x$link, "\n")
+    if (!is.null(x$type)) {
+      cat("Type:", x$type, "\n") 
     }
-    for (ap in names(ap_links)) {
-      cat(paste0("Link function of ", ap, ": ", ap_links[[ap]], " \n")) 
+    if (isTRUE(links) || is.character(links)) {
+      ap_links <- x[grepl("^link_", names(x))]
+      names(ap_links) <- sub("^link_", "", names(ap_links))
+      if (is.character(links)) {
+        ap_links <- rmNULL(ap_links[links])
+      }
+      for (ap in names(ap_links)) {
+        cat(paste0("Link function of ", ap, ": ", ap_links[[ap]], " \n")) 
+      }
+    }
+   
+  }
+  if (newline) {
+    cat("\n")
+  }
+  invisible(x)
+}
+
+#' @export
+print.mixfamily <- function(x, summary = FALSE, newline = TRUE, ...) {
+  if (summary) {
+    cat("mixture(")
+    for (i in seq_along(x$mix)) {
+      print(x$mix[[i]], summary = TRUE, newline = FALSE, ...)
+      if (i < length(x$mix)) {
+        cat(", ") 
+      }
+    }
+    cat(")")
+  } else {
+    cat("\nMixture\n")
+    for (i in seq_along(x$mix)) {
+      print(x$mix[[i]], newline = FALSE, ...)
     }
   }
-  cat("\n")
+  if (newline) {
+    cat("\n")
+  }
   invisible(x)
 }
 
