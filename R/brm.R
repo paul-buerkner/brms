11f4a83a5576f13676c89de6828778f58e6e83f6
Paul-Christian Bürkner <paul.buerkner@gmail.com>
2017-06-10 15:35:35 +0200

    Improve parallel execution flexibility via the 'future' package.

--- R/brm.R
+++ R/brm.R
@@ -140,6 +140,11 @@
 #'   It defaults to \code{NULL} so all the default values are used. 
 #'   The most important control parameters are discussed in the 'Details'
 #'   section below. For a comprehensive overview see \code{\link[rstan:stan]{stan}}.
+#' @param future Logical; If \code{TRUE}, the \pkg{\link[future:future]{future}}
+#'   package is used for parallel execution of the chains and argument \code{cores}
+#'   will be ignored. Can be set globally for the current \R session via the 
+#'   \code{future} option. The execution type is controlled via 
+#'   \code{\link[future:plan]{plan}} (see the examples section below).
 #' @param silent logical; If \code{TRUE} (the default), most of the
 #'   informational messages of compiler and sampler are suppressed.
 #'   The actual sampling progress is still printed. 
@@ -317,7 +322,11 @@
 #'             family = asym_laplace())
 #' summary(fit7)
 #' marginal_effects(fit7)
-#'    
+#' 
+#' ## use the future package for parallelization
+#' library(future)
+#' plan(multiprocess)
+#' fit7 <- update(fit7, future = TRUE)
 #' }
 #' 
 #' @import rstan
@@ -335,8 +344,8 @@ brm <- function(formula, data, family = gaussian(), prior = NULL,
                 chains = 4, iter = 2000, warmup = floor(iter / 2),
                 thin = 1, cores = getOption("mc.cores", 1L), control = NULL,
                 algorithm = c("sampling", "meanfield", "fullrank"),
-                silent = TRUE, seed = 12345, save_model = NULL,
-                save_dso = TRUE, ...) {
+                future = getOption("future", FALSE), silent = TRUE, 
+                seed = 12345, save_model = NULL, save_dso = TRUE, ...) {
   
   dots <- list(...) 
   # use deprecated arguments if specified
@@ -364,7 +373,7 @@ brm <- function(formula, data, family = gaussian(), prior = NULL,
     # re-use existing model
     x <- fit
     # compute data to be passed to Stan
-    standata <- standata(x, is_newdata = dots$is_newdata)
+    sdata <- standata(x, is_newdata = dots$is_newdata)
     dots$is_newdata <- NULL
     # extract the compiled model
     x$fit <- rstan::get_stanmodel(x$fit)
@@ -413,9 +422,9 @@ brm <- function(formula, data, family = gaussian(), prior = NULL,
       stan_funs = stan_funs, save_model = save_model, 
       brm_call = TRUE, silent = silent
     )
-    # generate standata before compiling the model to avoid
+    # generate Stan data before compiling the model to avoid
     # unnecessary compilations in case of invalid data
-    standata <- standata(x, newdata = dots$is_newdata)
+    sdata <- standata(x, newdata = dots$is_newdata)
     message("Compiling the C++ model")
     x$fit <- eval_silent(
       rstan::stan_model(stanc_ret = x$model, save_dso = save_dso)
@@ -427,22 +436,48 @@ brm <- function(formula, data, family = gaussian(), prior = NULL,
   if (is.character(inits) && !inits %in% c("random", "0")) {
     inits <- get(inits, mode = "function", envir = parent.frame())
   }
-  args <- nlist(object = x$fit, data = standata, pars = x$exclude, 
-                include = FALSE, algorithm, iter)
-  args[names(dots)] <- dots 
-  if (algorithm == "sampling") {
-    args <- c(args, 
-      nlist(init = inits, warmup, thin, chains, cores, 
-            control, show_messages = !silent)
-    )
-  }
+  args <- nlist(
+    object = x$fit, data = sdata, pars = x$exclude, 
+    include = FALSE, algorithm, iter
+  )
+  args[names(dots)] <- dots
   
   set.seed(seed)
   message("Start sampling")
   if (args$algorithm == "sampling") {
     args$algorithm <- NULL
-    x$fit <- do.call(rstan::sampling, args)
+    args <- c(args,
+      nlist(init = inits, warmup, thin, control, show_messages = !silent)
+    )
+    if (future) {
+      if (!requireNamespace("future", quietly = TRUE)) {
+        stop2("Please install the 'future' package.")
+      }
+      if (cores > 1L) {
+        warning("Argument 'cores' is ignored when using 'future'.")
+      }
+      args$chains <- 1L
+      futures <- fits <- vector("list", chains)
+      for (i in seq_len(chains)) {
+        args$chain_id <- i
+        if (is.list(inits)) {
+          args$init <- inits[i]
+        }
+        futures[[i]] <- future::future(
+          do.call(rstan::sampling, args), packages = "rstan"
+        )
+      }
+      for (i in seq_len(chains)) {
+        fits[[i]] <- future::value(futures[[i]]) 
+      }
+      x$fit <- rstan::sflist2stanfit(fits)
+      rm(futures, fits)
+    } else {
+      args <- c(args, nlist(chains, cores))
+      x$fit <- do.call(rstan::sampling, args) 
+    }
   } else {
+    # vb does not support parallel execution
     x$fit <- do.call(rstan::vb, args)
   }
   if (!isTRUE(testmode)) {
