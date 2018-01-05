4527130ed73e26452783be3251276e3626c44de9
Paul-Christian Bürkner <paul.buerkner@gmail.com>
2018-01-05 13:25:55 +0100

    fix issue #310

--- R/misc-methods.R
+++ R/misc-methods.R
@@ -132,7 +132,7 @@ print.brmshypothesis <- function(x, digits = 2, chars = 20, ...) {
   x$hypothesis[, 1:5] <- round(x$hypothesis[, 1:5], digits = digits)
   print(x$hypothesis, quote = FALSE)
   cat(paste0("---\n'*': The expected value under the hypothesis ", 
-             "lies outside the ", (1 - x$alpha) * 100, "% CI."))
+             "lies outside the ", (1 - x$alpha) * 100, "% CI.\n"))
   invisible(x)
 }
 
