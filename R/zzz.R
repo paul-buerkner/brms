9041f4128ad932d11a648122c93b977f884126d5
Russell V. Lenth <russell-lenth@uiowa.edu>
2020-05-13 17:33:37 -0500

    Added emmeans support

--- R/zzz.R
+++ R/zzz.R
@@ -19,3 +19,14 @@
 #   rstan::stan_model(stanc_ret = model)
 # }
 # new_stan_functions <- new_stan_functions()
+
+
+# The following code dynamically registers the recover_data and emm_basis
+# methods needed by emmeans, if that package is installed.
+
+.onLoad <- function(libname, pkgname) {
+    if (requireNamespace("emmeans", quietly = TRUE)) {
+        emmeans::.emm_register(c("brmsfit"), pkgname)
+    }
+    invisible()
+}
