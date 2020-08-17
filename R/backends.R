d9e2debc339c825c332f56b262f1dc61a51e5d82
jgabry <jgabry@gmail.com>
2020-08-17 09:46:01 -0600

    write_stan_tempfile -> write_stan_file

--- R/backends.R
+++ R/backends.R
@@ -25,7 +25,7 @@ parse_model <- function(model, backend, ...) {
 # @return validated Stan model code
 .parse_model_cmdstanr <- function(model, silent = TRUE, ...) {
   require_package("cmdstanr")
-  temp_file <- cmdstanr::write_stan_tempfile(model)
+  temp_file <- cmdstanr::write_stan_file(model)
   out <- eval_silent(
     cmdstanr::cmdstan_model(temp_file, compile = FALSE, ...),
     type = "message", 
@@ -60,7 +60,7 @@ compile_model <- function(model, backend, ...) {
 .compile_model_cmdstanr <- function(model, ...) {
   require_package("cmdstanr")
   args <- list(...)
-  args$stan_file <- cmdstanr::write_stan_tempfile(model)
+  args$stan_file <- cmdstanr::write_stan_file(model)
   do_call(cmdstanr::cmdstan_model, args)
 }
 
