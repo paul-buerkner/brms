fb464369fb52d4eb94e12abd14d7ef6f79bf37df
fweber144 <fweber144@protonmail.com>
2021-05-12 14:33:43 +0200

    Fix a test (do not drop a single observation if the input object was a 3-dimensional array).

--- tests/testthat/helpers/inv_link_categorical_fun.R
+++ tests/testthat/helpers/inv_link_categorical_fun.R
@@ -1,21 +1,25 @@
+# Very similar to inv_link_categorical(), but iterates over the observations:
 inv_link_categorical_ch <- function(x, has_refcat = TRUE, log = FALSE) {
   ndim <- length(dim(x))
   # For testing purposes, only allow 3-dimensional arrays here:
   if (ndim <= 1) {
     x <- array(x, dim = c(1, 1, length(x)))
     ndim <- length(dim(x))
+    need_drop <- TRUE
   } else if (ndim == 2) {
     x <- array(x, dim = c(dim(x)[1], 1, dim(x)[2]))
     ndim <- length(dim(x))
+    need_drop <- TRUE
   } else if (ndim > 3) {
     stop("At most 3 dimensions are allowed here.")
+  } else {
+    need_drop <- FALSE
   }
   if (!has_refcat) {
     dim_noncat <- dim(x)[-ndim]
     zeros_arr <- array(0, dim = c(dim_noncat, 1))
     x <- abind::abind(zeros_arr, x)
   }
-  # For testing purposes, iterate over the observations:
   ndraws <- dim(x)[1]
   nobsv <- dim(x)[2]
   ncat <- dim(x)[3]
@@ -33,8 +37,9 @@ inv_link_categorical_ch <- function(x, has_refcat = TRUE, log = FALSE) {
     ),
     perm = c(1, 3, 2)
   )
-  # Quick-and-dirty solution to drop the margin for a single observation:
-  if (nobsv == 1) {
+  # Quick-and-dirty solution to drop the margin for a single observation (but
+  # only if the input object was not a 3-dimensional array):
+  if (need_drop) {
     return(slice(out, 2, 1))
   }
   out
