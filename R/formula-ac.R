78b975341e49dcfd18a10d2bc46de405be425d14
Adam Peterson <adam@adams-mac-mini.lan>
2021-09-16 15:21:53 -0400

    Fix binary check in validate_car_matrix()

--- R/formula-ac.R
+++ R/formula-ac.R
@@ -610,10 +610,11 @@ validate_car_matrix <- function(M) {
     stop2("'M' for CAR terms must be symmetric.")
   }
   colnames(M) <- rownames(M)
-  not_binary <- !(M == 0 | M == 1)
+  vals <- unique(M@x) # all unique nonzero values
+  not_binary <- !(vals == 1)
   if (any(not_binary)) {
     message("Converting all non-zero values in 'M' to 1.")
-    M[not_binary] <- 1
+    M@x[not_binary] <- 1
   }
   M
 }
