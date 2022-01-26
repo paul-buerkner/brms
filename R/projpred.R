1208498734619743597aa94ec1e9523790c23fb7
fweber144 <fweber144@protonmail.com>
2022-01-26 15:30:08 +0100

    Add a warning for projpred version <= 2.0.2.

--- R/projpred.R
+++ R/projpred.R
@@ -164,6 +164,11 @@ get_refmodel.brmsfit <- function(object, newdata = NULL, resp = NULL,
     # }
   }
   
+  if (utils::packageVersion("projpred") <= "2.0.2") {
+    warning2("Under projpred version <= 2.0.2, projpred's K-fold CV results ",
+             "may not be reproducible for multilevel brms reference models.")
+  }
+  
   # extract a list of K-fold sub-models
   if (is.null(cvfun)) {
     cvfun <- function(folds, ...) {
