8cf0df227dff5f7cfe83175d3f15e147a7158b97
fweber144 <fweber144@protonmail.com>
2022-01-29 16:02:53 +0100

    Check for a multilevel model before throwing a warning concerning
    unreproducible K-fold CV results.

--- R/projpred.R
+++ R/projpred.R
@@ -164,7 +164,7 @@ get_refmodel.brmsfit <- function(object, newdata = NULL, resp = NULL,
     # }
   }
   
-  if (utils::packageVersion("projpred") <= "2.0.2") {
+  if (utils::packageVersion("projpred") <= "2.0.2" && NROW(object$ranef)) {
     warning2("Under projpred version <= 2.0.2, projpred's K-fold CV results ",
              "may not be reproducible for multilevel brms reference models.")
   }
