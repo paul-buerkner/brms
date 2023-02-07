b8bcaa0a9d0ff6218f23e4f743f4fd8a3dc5f081
yananlong <yanan.long439@gmail.com>
2023-02-07 00:37:17 -0600

    minor changes

--- tests/local/tests.models-6.R
+++ tests/local/tests.models-6.R
@@ -20,4 +20,23 @@ test_that("Mixture cure models work correctly", {
     print(prior_summary(fit1))
     expect_range(posterior_summary(fit1)["b_Intercept", "Estimate"], 4.9, 5.1)
     expect_range(posterior_summary(fit1)["b_inc_Intercept", "Estimate"], 0.95, 1.05)
+    
+    fit2 <- brm(
+        formula = bf(
+            time | cens(1 - cens) ~ transplant,
+            inc ~ transplant
+        ),
+        data = leukaemia, family = mixcure_weibull(),
+        prior = c(set_prior("normal(0, 10)", class = "b"),
+                  set_prior("normal(0, 10)", class = "b", dpar = "inc")
+                 ),
+        save_pars = save_pars(all = TRUE),
+        warmup = 1000, iter = 2000, refresh = 2000, 
+        chains = 8, cores = 8,
+        control = list(adapt_delta = 0.825),
+    )
+    print(fit2, digits = 3)
+    print(prior_summary(fit2))
+    expect_range(posterior_summary(fit2)["b_Intercept", "Estimate"], 4.9, 5.1)
+    expect_range(posterior_summary(fit2)["b_inc_Intercept", "Estimate"], 0.95, 1.05)
 })
