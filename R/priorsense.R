ffea82a0494c62a5d6fdb1c53aa1dbfa68a2049d
Paul-Christian Bürkner <paul.buerkner@gmail.com>
2024-06-06 11:54:04 +0200

    minor cleaning and renaming of functions

--- R/priorsense.R
+++ R/priorsense.R
@@ -8,7 +8,7 @@
 #' functions, so you will rarely need to call it manually yourself.
 #'
 #' @param x A \code{brmsfit} object.
-#' @param ... Unused.
+#' @param ... Currently unused.
 #'
 #' @return A \code{priorsense_data} object to be used in conjunction
 #' with the \pkg{priorsense} package.
@@ -24,70 +24,61 @@
 #' # The following code requires the 'priorsense' package to be installed:
 #' library(priorsense)
 #'
+#' # perform powerscaling of the prior
+#' powerscale(fit, alpha = 1.5, component = "prior")
+#'
 #' # perform powerscaling sensitivity checks
 #' powerscale_sensitivity(fit)
 #'
 #' # create powerscaling sensitivity plots
 #' powerscale_plot_dens(fit)
 #' }
+#'
 #' @exportS3Method priorsense::create_priorsense_data brmsfit
 create_priorsense_data.brmsfit <- function(x, ...) {
-
-  create_priorsense_data.default(
-    x = get_draws_brmsfit(x),
+  priorsense::create_priorsense_data.default(
+    x = get_draws_ps(x),
     fit = x,
-    log_prior = log_prior_draws.brmsfit(x),
-    log_lik = log_lik_draws.brmsfit(x),
-    log_prior_fn = log_prior_draws,
-    log_lik_fn = log_lik_draws,
-    log_ratio_fn = powerscale_log_ratio_fun_brmsfit,
+    log_prior = log_prior_ps(x),
+    log_lik = log_lik_ps(x),
+    log_prior_fn = log_prior_ps,
+    log_lik_fn = log_lik_ps,
+    log_ratio_fn = powerscale_log_ratio,
     ...
   )
 }
 
-##' @exportS3Method priorsense::log_lik_draws brmsfit
-log_lik_draws.brmsfit <- function(x) {
+log_lik_ps <- function(x) {
   log_lik <- log_lik(x)
   log_lik <- posterior::as_draws_array(log_lik)
   posterior::variables(log_lik) <- paste0("log_lik[", 1:nvariables(log_lik), "]")
-
-  return(log_lik)
+  log_lik
 }
 
-
-##' @exportS3Method priorsense::log_prior_draws brmsfit
-log_prior_draws.brmsfit <- function(x, log_prior_name = "lprior") {
-
-  log_prior <- posterior::subset_draws(
+log_prior_ps <- function(x, log_prior_name = "lprior") {
+  posterior::subset_draws(
     posterior::as_draws_array(x),
     variable = log_prior_name
   )
-
-  return(log_prior)
 }
 
-get_draws_brmsfit <- function(x, variable = NULL, regex = FALSE, log_prior_name = "lprior") {
-
+get_draws_ps <- function(x, variable = NULL, regex = FALSE,
+                         log_prior_name = "lprior") {
   excluded_variables <- c(log_prior_name, "lp__")
   draws <- posterior::as_draws_df(x, regex = regex)
-
   if (is.null(variable)) {
     # remove unnecessary variables
     variable <- posterior::variables(x)
     variable <- variable[!(variable %in% excluded_variables)]
-
     draws <- posterior::subset_draws(draws, variable = variable)
   }
-
-  return(draws)
+  draws
 }
 
-powerscale_log_ratio_fun_brmsfit <- function(draws, fit, alpha, component_fn) {
-
+powerscale_log_ratio <- function(draws, fit, alpha, component_fn) {
   component_draws <- component_fn(fit)
   component_draws <- rowsums_draws(component_draws)
   component_draws * (alpha - 1)
-
 }
 
 rowsums_draws <- function(x) {
