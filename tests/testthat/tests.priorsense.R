25aa0b65fa78600cffaeb744c8985a14213da45e
Paul-Christian Bürkner <paul.buerkner@gmail.com>
2024-06-24 10:44:12 +0200

    fix typo in tests

--- tests/testthat/tests.priorsense.R
+++ tests/testthat/tests.priorsense.R
@@ -8,13 +8,13 @@ fit1 <- rename_pars(brms:::brmsfit_example1)
 
 test_that("create_priorsense_data returns expected output structure", {
   psd1 <- create_priorsense_data(fit1)
-  expect_s3_class(psd$draws, "draws")
-  expect_s3_class(psd$fit, "brmsfit")
-  expect_s3_class(psd$log_lik, "draws")
-  expect_s3_class(psd$log_prior, "draws")
-  expect_true(is.function(psd$log_lik_fn))
-  expect_true(is.function(psd$log_prior_fn))
-  expect_true(is.function(psd$log_ratio_fn))
+  expect_s3_class(psd1$draws, "draws")
+  expect_s3_class(psd1$fit, "brmsfit")
+  expect_s3_class(psd1$log_lik, "draws")
+  expect_s3_class(psd1$log_prior, "draws")
+  expect_true(is.function(psd1$log_lik_fn))
+  expect_true(is.function(psd1$log_prior_fn))
+  expect_true(is.function(psd1$log_ratio_fn))
 })
 
 test_that("powerscale returns without error", {
