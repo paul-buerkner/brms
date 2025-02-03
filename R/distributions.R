d6050484e3a4f9d40b26ba6232de51141617eb2a
Tom Peatman <tom.peatman@gmail.com>
2025-02-03 11:56:11 +0000

    update ddirichletmultinomial
    add checks for dimensions of eta, and improve documentation

--- R/distributions.R
+++ R/distributions.R
@@ -2185,12 +2185,18 @@ dmultinomial <- function(x, eta, log = FALSE) {
   out
 }
 
-# density of the multinomial distribution with the softmax transform
+# density of the dirichlet-multinomial distribution with the softmax transform
 # @param x positive integers not greater than ncat
 # @param eta the linear predictor (of length or ncol ncat)
-# @param phi the dispersion parameter
+# @param phi the dispersion parameter (i.e., sum of dirichlet alphas)
 # @param log return values on the log scale?
 ddirichletmultinomial <- function(x, eta, phi, log = FALSE) {
+  if (is.null(dim(eta))) {
+    eta <- matrix(eta, nrow = 1)
+  }
+  if (length(dim(eta)) != 2L) {
+    stop2("eta must be a numeric vector or matrix.")
+  }
   require_package("extraDistr")
   alpha <- softmax(eta) * phi
   size <- sum(x)
