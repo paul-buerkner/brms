cf9c92fa1f22bce402ce475669cb5a0fea0cc8ef
Sermet Pekin <sermet.pekin@gmail.com>
2025-06-30 21:49:48 +0300

    fixed faling test for create_filename_auto func

--- tests/testthat/tests.create_filename_auto.R
+++ tests/testthat/tests.create_filename_auto.R
@@ -9,13 +9,17 @@ test_that(" create_filename_auto works", {
   a  <- create_filename_auto( "a" , "b"  , F , nlist(a  = 5 , family = gaussian()  ))
   a_exp <- nlist( file = "a"  , file_refit = "b" )
   expect_equal(a , a_exp )
+
   a  <- create_filename_auto( "a" , "b"  , T , nlist(a  = 5 , family = gaussian()  ))
-  a_exp <- nlist( file = "cache-brm-result_dcdc977f8f7c789f.Rds"   , file_refit = "on_change" )
+  a_exp <- nlist( file =  "cache-brm-result_a1d7ed8d4b70cd32.Rds"   , file_refit = "on_change" )
   expect_equal(a , a_exp )
+
   a  <- create_filename_auto( "a" , "b"  , T , nlist(a  = 5 , family = student()  ))
-  a_exp <- nlist( file =  "cache-brm-result_dcdc977f8f7c789f.Rds"    , file_refit = "on_change" )
+  a_exp <- nlist( file =  "cache-brm-result_a1d7ed8d4b70cd32.Rds"    , file_refit = "on_change" )
   expect_equal(a , a_exp )
+
+
   a  <- create_filename_auto( NULL , "something"  , T , nlist(a  = 5 , family = gaussian()  ))
-  a_exp <- nlist( file =  "cache-brm-result_dcdc977f8f7c789f.Rds"  , file_refit = "on_change" )
+  a_exp <- nlist( file =  "cache-brm-result_a1d7ed8d4b70cd32.Rds"  , file_refit = "on_change" )
   expect_equal(a , a_exp )
 })
