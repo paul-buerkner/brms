8fe25d4af907c21caa16ea185b93414001f21f59
Sermet Pekin <sermet.pekin@gmail.com>
2025-07-01 21:22:03 +0300

    tests fixed

--- tests/testthat/tests.brm_file_auto.R
+++ tests/testthat/tests.brm_file_auto.R
@@ -1,29 +1,12 @@
-
-# file_auto
-test_that("file_auto option works", {
-   skip("Temporarily disabled for debugging reasons")
-  # test_cache_dir <- tempdir()
-  # options(brms.cache_folder = test_cache_dir)
-    epilepsy2 <- epilepsy[-c(1), ]
-    # same
-    f1 <- brm_mock_for_file_auto(count ~ zAge + zBase * Trt + (1|patient),
-                           data = epilepsy, family = poisson() ,    file_auto = TRUE)
-    f2 <- brm_mock_for_file_auto(count ~ zAge + zBase * Trt + (1|patient),
-                                 data = epilepsy, family = gaussian() ,    file_auto = TRUE)
-    expect_equal(f1$file, f2$file)
-    # different data
-    f1 <- brm_mock_for_file_auto(count ~ zAge + zBase * Trt + (1|patient),
-                                 data = epilepsy, family = poisson(), file_auto = TRUE)
-    f2 <- brm_mock_for_file_auto(count ~ zAge + zBase * Trt + (1|patient),
-                                 data = epilepsy2, family = poisson(), file_auto = TRUE)
-    expect_false(f1$file == f2$file)
-    # different family
-    f1 <- brm_mock_for_file_auto(count ~ zAge + zBase * Trt + (1|patient),
-                                 data = epilepsy, family = poisson(), file_auto = TRUE)
-    f2 <- brm_mock_for_file_auto(count ~ zAge + zBase * Trt + (1|patient),
-                                 data = epilepsy, family = gaussian(), file_auto = TRUE)
-    expect_equal(f1$file, f2$file)
-})
+# toy objects reused across tests
+f1 <- mpg ~ wt
+f2 <- mpg ~ wt + cyl            # same data, different formula
+d1 <- mtcars
+d2 <- mtcars[sample(nrow(mtcars)), ]   # same rows, shuffled order
+fam1 <- gaussian()
+fam2 <- student()
+base <- nlist(formula = f1, data = d1, family = fam1)
+h_base <- hash_dots(base, data_policy = "hash")
 
 test_that("hash function check" , {
   aa1 =   hash_dots( count ~  zAge + zBase * Trt + (1|patient),
@@ -66,15 +49,7 @@ test_that("formula environment ignored", {
 })
 
 test_that("changing *one* statistical input changes the hash", {
-  # toy objects reused across tests
-  f1 <- mpg ~ wt
-  f2 <- mpg ~ wt + cyl            # same data, different formula
-  d1 <- mtcars
-  d2 <- mtcars[sample(nrow(mtcars)), ]   # same rows, shuffled order
-  fam1 <- gaussian()
-  fam2 <- student()
-  base <- nlist(formula = f1, data = d1, family = fam1)
-  h_base <- hash_dots(base, data_policy = "hash")
+
   # 1. Different formula
   h_formula <- hash_dots(
     modifyList(base, list(formula = f2)), data_policy = "hash"
