b543a7ba7f5d6795e84d3e0eabfff8d8f47ddc06
sermet.pekin <Sermet.Pekin@gmail.com>
2025-07-07 15:28:10 +0300

    remove file_auto parameter for this PR

--- tests/testthat/tests.brm_call.R
+++ tests/testthat/tests.brm_call.R
@@ -1,13 +1,10 @@
 test_that("create_brm_call and call_only works", {
-
-  file_auto <- TRUE
   # `call_only = T` creates a brm_call
   call_1 <- brm(count ~ zAge + zBase * Trt + (1|patient),
-                data = epilepsy, family = poisson(), call_only = T, file_auto = file_auto)
+                data = epilepsy, family = poisson(), call_only = T)
   # alternatively create_brm_call creates a brm_call without redefining parameters
   call_2  <- create_brm_call(count ~ zAge + zBase * Trt + (1|patient),
-                             data = epilepsy, family = poisson(),
-                             file_auto = file_auto)
+                             data = epilepsy, family = poisson())
 
   expect_equal(all.equal(call_1 , call_2), TRUE)
   c1 <- brm(call_1, call_only = TRUE)
@@ -16,22 +13,20 @@ test_that("create_brm_call and call_only works", {
 })
 
 test_that("brm_call objects are consistent", {
-  file_auto <- TRUE
   call_1 <- brm(count ~ zAge + zBase * Trt + (1|patient),
-                data = epilepsy, family = poisson(), call_only = T, file_auto = file_auto)
+                data = epilepsy, family = poisson(), call_only = T)
   call_2  <- create_brm_call(count ~ zBase * Trt + (1|patient),
-                             data = epilepsy, family = poisson(), file_auto = file_auto)
+                             data = epilepsy, family = poisson())
   expect_no_error(print(call_1))
   expect_no_error(print(call_2))
   expect_true(as.character(call_1$formula)[[3]] != as.character(call_2$formula)[[3]])
 })
 
 test_that("brm_call objects are consistent", {
-  file_auto <- TRUE
   call_1 <- brm(count ~ zAge + zBase * Trt + (1|patient),
-                data = epilepsy, family = poisson(), call_only = T, file_auto = file_auto)
+                data = epilepsy, family = poisson(), call_only = T)
   call_2 <- brm(count ~ zAge + zBase * Trt + (1|patient),
-                data = epilepsy, family = poisson(), call_only = T, file_auto = file_auto)
+                data = epilepsy, family = poisson(), call_only = T)
   expect_no_error(print(call_1))
   expect_no_error(print(call_2))
   expect_true(as.character(call_1$formula)[[3]] == as.character(call_2$formula)[[3]])
