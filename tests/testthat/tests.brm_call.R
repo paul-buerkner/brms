04cfdaa20b2eaa0583ed0b762a53479e0f59ecc9
sermet.pekin <Sermet.Pekin@gmail.com>
2025-07-07 15:17:03 +0300

    remove hash check from tests

--- tests/testthat/tests.brm_call.R
+++ tests/testthat/tests.brm_call.R
@@ -37,14 +37,3 @@ test_that("brm_call objects are consistent", {
   expect_true(as.character(call_1$formula)[[3]] == as.character(call_2$formula)[[3]])
 })
 
-test_that("brm_call objects are consistent", {
-  # brm_call arguments will be checked and modify inside `brm()`function
-  # therefore e.g. call_1$threads will ba a list containing threads etc.
-  file_auto <- TRUE
-  call_1 <- brm(count ~ zAge + zBase * Trt + (1|patient),
-                data = epilepsy, family = poisson() , call_only = T, file_auto = file_auto)
-  expect_true(!is.null(call_1$model_hash))
-  expect_no_error(print(call_1))
-  expect_true(all(is.null(call_1$data2) , call_1$iter == 2000, call_1$normalize,
-                   call_1$warmup == 1000, is.null(call_1$threads$threads)))
-})
